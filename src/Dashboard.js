c1f8a2bd74d971f82cf6e2af5314234c6f7a7fbc
Liu <Siyuan.Liu12@T-Mobile.com>
2020-01-23 17:56:42 -0500

    created divs

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,77 +9,80 @@ import  './Dashboard.css';
 function Dashboard() {
     return (
         <html lang="en">
+
         <head>
             <title>Dashboard</title>
         </head>
 
         <body>
-        <div className="sidenav">
-            <h3> Welcome Ally!</h3>
-            <a>Dashboard</a>
-            <a href="#">Announcements</a>
-            <a href="#">Lessons</a>
-            <a href="#">Quizzes</a>
-            <a href="#">Videos</a>
-            <a href="#">Tactics</a>
-        </div>
-
-        <div className="title">
-            <h1>Dashboard</h1>
-        </div>
-
-        <div className="content">
-            <p>This Week's Lesson:</p>
-            <div className="boxes">
-                <h2>Moving Pieces <button>View</button> </h2>
-
-            </div>
-            <p>Daily Tactic:</p>
-            <div className="boxes">
-                <h2>Pawns <button >View</button></h2>
-
+            <div className="sidenav">
+                <h3> Welcome Ally!</h3>
+                <a>Dashboard</a>
+                <a href="#">Announcements</a>
+                <a href="#">Lessons</a>
+                <a href="#">Quizzes</a>
+                <a href="#">Videos</a>
+                <a href="#">Tactics</a>
             </div>
 
-            <table width="100%">
-                <tr>
-                    <th colSpan="3">Announcements</th>
-                </tr>
-                <tr>
-                    <td className="date">10/17/19</td>
-                    <td>Hi Class! Remember to bring your pamphlet to class and we'll be testing out
-                        some new chess strategies with...
-                    </td>
+            <div className="content">
 
-                </tr>
-                <tr>
-                    <td className="date">10/15/19</td>
-                    <td>Don't forget to do your homework! It's due by 11:59pm and I will not be giving
-                        out homework extensions si...
-                    </td>
+                <div className="title">
+                    <h1>Dashboard</h1>
+                </div>
 
-                </tr>
-            </table>
-            <table width="100%">
-                <tr>
-                    <th colSpan="2">Quizzes</th>
-                </tr>
-                <tr>
-                    <td className="date">10/17/19</td>
-                    <td>Week 1: Checkmate</td>
+                <div className = "top_section">
+                <p>This Week's Lesson:</p>
+                <div className="boxes">
+                    <h2>Moving Pieces <button>View</button> </h2>
 
-                </tr>
-                <tr>
-                    <td className="date">10/24/19</td>
-                    <td>Week 2: Pieces</td>
+                </div>
+                <p>Daily Tactic:</p>
+                <div className="boxes">
+                    <h2>Pawns <button >View</button></h2>
 
-                </tr>
-                <tr>
-                    <td className="date">10/31/19</td>
-                    <td>Week 3: Fork</td>
+                </div>
+                </div>
+                <table width="100%">
+                    <tr>
+                        <th colSpan="3">Announcements</th>
+                    </tr>
+                    <tr>
+                        <td className="date">10/17/19</td>
+                        <td>Hi Class! Remember to bring your pamphlet to class and we'll be testing out
+                            some new chess strategies with...
+                        </td>
 
-                </tr>
-            </table>
-        </div>
+                    </tr>
+                    <tr>
+                        <td className="date">10/15/19</td>
+                        <td>Don't forget to do your homework! It's due by 11:59pm and I will not be giving
+                            out homework extensions si...
+                        </td>
+
+                    </tr>
+                </table>
+                <table width="100%">
+                    <tr>
+                        <th colSpan="2">Quizzes</th>
+                    </tr>
+                    <tr>
+                        <td className="date">10/17/19</td>
+                        <td>Week 1: Checkmate</td>
+
+                    </tr>
+                    <tr>
+                        <td className="date">10/24/19</td>
+                        <td>Week 2: Pieces</td>
+
+                    </tr>
+                    <tr>
+                        <td className="date">10/31/19</td>
+                        <td>Week 3: Fork</td>
+
+                    </tr>
+                </table>
+            </div>
 
         </body>
         </html>
