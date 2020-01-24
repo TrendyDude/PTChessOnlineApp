757e827e7c84029160ff98a61ab2d0e53557e0f4
dante allen <dallen68@gatech.edu>
2020-01-23 20:03:48 -0500

    Create Account Page added linked login to dashboard and create account

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import './Login.css';
+import CreateAccount from "./CreateAccount";
+import Dashboard from "./Dashboard";
 
 function Login() {
     return(
@@ -13,21 +15,28 @@ function Login() {
         </head>
 
         <body>
-            <div className="content">
+            <div className="contentLogin">
                 <h3>Member Login</h3>
                 <form>
                     <input type="text" name="username" placeholder="Username"/>
                     <input type="text" name="password" placeholder="Password"/>
                 </form>
-                <button type="button">Login</button>
+                <div className="loginbutton">
+                    <button type="button" onClick={gotoDashboard}>Login</button>
+                </div>
                 <a href="#">Forgot username/password?</a>
                 <div className="signup">
-                    <p>Dont have an account? <a href="#">Sign up!</a></p>
-
+                    <p>Dont have an account?<button onClick={gotoCreateAccount}>Sign up!</button></p>
                 </div>
             </div>
         </body>
         </html>
     );
 }
-export default Login
+function gotoCreateAccount() {
+    ReactDOM.render(<CreateAccount/>, document.getElementById('root'));
+}
+function gotoDashboard() {
+    ReactDOM.render(<Dashboard/>, document.getElementById('root'));
+}
+export default Login;
