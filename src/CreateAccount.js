ab1c3620d03012af9f00818c99aa03f5d8e6e425
TrendyDude <tlsearcy@gatech.edu>
2020-01-24 14:53:16 -0500

    Merge branch 'master' of https://github.com/TrendyDude/PTChessOnlineApp Updated Dashboard # Conflicts: #	README.md

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -18,9 +18,10 @@ function CreateAccount() {
         <div className="contentAcc">
             <h3>Create Account</h3>
             <form>
-                <input type="text" name="email" placeholder="Your Email"/>
-                <input type="password" name="password" placeholder="New Password"/>
+                <input type="text" name="username" id="user" placeholder="Username"/>
+                <input type="password" name="password" id="pass" placeholder="New Password"/>
                 <input type="password" name="confirm_password" placeholder="Confirm Password"/>
+                <input type="text" name="username" id="user" placeholder="Username"/>
 
 
             </form>
@@ -41,5 +42,42 @@ function gotoMember() {
 }
 function gotoLogin() {
     ReactDOM.render(<Login/>, document.getElementById('root'));
+}
+
+function addAccount(user, password, first, last,) {
+    var AWS = require("aws-sdk");
+
+    AWS.config.update({
+        region: "us-east-1",
+        endpoint: "https://dynamodb.us-east-1.amazonaws.com",
+        accessKeyId: "AKIA2F56XJ6UFLFPYJLS",                                           //make private
+        secretAccessKey: "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"
+    });
+
+
+
+    var ddb = new AWS.DynamoDB({apiVersion: "2012-08-10"});
+
+
+
+    var params = {
+        TableName:"Users",
+        Item: {
+            "Username":{S: user},
+            "FirstName":{S: first},
+            "Password":{S: password},
+            "Usertype":{S: usertype},
+            "LastName":{S: last},
+
+        }
+
+    };
+    ddb.putItem(params, function (err, data) {
+        if(err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
 }
 export default CreateAccount;
