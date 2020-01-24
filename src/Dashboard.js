247f1ed07bba395a10491ca1d9a5b5faffffde93
Liu <Siyuan.Liu12@T-Mobile.com>
2020-01-23 20:04:32 -0500

    dashboard redone

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -31,57 +31,46 @@ function Dashboard() {
                     <h1>Dashboard</h1>
                 </div>
 
-                <div className = "top_section">
-                <p>This Week's Lesson:</p>
-                <div className="boxes">
-                    <h2>Moving Pieces <button>View</button> </h2>
-
+                <div className="top_section">
+                    <div className="this_weeks_lesson">
+                        <p id="headers">THIS WEEK'S LESSON:</p>
+                        <p id="pink_titles">Moving Pieces</p>
+                        <svg width="300" height="120">
+                            <rect id="rect1" width="300" height="120" rx="15"/>
+                        </svg>
+                    </div>
+                    <div className="tactic">
+                        <p id="headers">DAILY TACTICS:</p>
+                        <p id="pink_titles">Pawns</p>
+                        <svg width="300" height="120">
+                            <rect id="rect1" width="300" height="120" rx="15"/>
+                        </svg>
+                    </div>
                 </div>
-                <p>Daily Tactic:</p>
-                <div className="boxes">
-                    <h2>Pawns <button >View</button></h2>
 
+                <div className="bottom_section">
+                    <div className="to_do">
+                        <p id="dark_headers">To Do:</p>
+                        <svg width="350" height="200">
+                            <rect id="todo_rect" width="350" height="200" rx="15"/>
+                        </svg>
+                    </div>
+                    <div className="announcement">
+                        <p id="dark_headers">New Announcements:</p>
+                        <p id="item">Hello</p>
+                        <svg width="400" height="50">
+                                <rect id="rect1" width="400" height="50" rx="15"/>
+                                <p>Hello</p>
+                        </svg>
+                        <svg width="400" height="50">
+                            <rect id="rect1" width="400" height="50" rx="15"/>
+                        </svg>
+                        <svg width="400" height="50">
+                            <rect id="rect1" width="400" height="50" rx="15"/>
+                        </svg>
+                    </div>
                 </div>
-                </div>
-                <table width="100%">
-                    <tr>
-                        <th colSpan="3">Announcements</th>
-                    </tr>
-                    <tr>
-                        <td className="date">10/17/19</td>
-                        <td>Hi Class! Remember to bring your pamphlet to class and we'll be testing out
-                            some new chess strategies with...
-                        </td>
 
-                    </tr>
-                    <tr>
-                        <td className="date">10/15/19</td>
-                        <td>Don't forget to do your homework! It's due by 11:59pm and I will not be giving
-                            out homework extensions si...
-                        </td>
-
-                    </tr>
-                </table>
-                <table width="100%">
-                    <tr>
-                        <th colSpan="2">Quizzes</th>
-                    </tr>
-                    <tr>
-                        <td className="date">10/17/19</td>
-                        <td>Week 1: Checkmate</td>
-
-                    </tr>
-                    <tr>
-                        <td className="date">10/24/19</td>
-                        <td>Week 2: Pieces</td>
-
-                    </tr>
-                    <tr>
-                        <td className="date">10/31/19</td>
-                        <td>Week 3: Fork</td>
-
-                    </tr>
-                </table>
             </div>
 
         </body>
